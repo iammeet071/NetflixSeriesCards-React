0763f38ae8a8a045903e0b72057d11872aff26e5
Meet Parmar <meetparmar071@gmail.com>
2021-04-24 22:15:42 +0530

    .

--- src/App.js
+++ src/App.js
@@ -9,71 +9,62 @@ function App() {
     <Header/>
     <div className="cardss">
     <Cards
-    vlinks='/videos/n.mp4'
-     imgsrc='https://images6.alphacoders.com/744/744722.png'
+     imgsrc='/images/1.png'
      sname='NETFLIX ANIME SERIES'
      ctitle='NARUTO SHIPPUDEN'
      clink='https://animeheaven.ru/detail/naruto-shippuuden-sub.36530'
      />
      <Cards
-     vlinks='/videos/ds.mp4'
-     imgsrc='https://images3.alphacoders.com/114/1142445.png'
+     imgsrc='/images/2.png'
      sname='NETFLIX ANIME SERIES'
      ctitle='DEMON SLAYER'
      clink='https://animeheaven.ru/detail/kimetsu-no-yaiba-sub.92845'
      />
      <Cards
-     vlinks='/videos/tg.mp4'
-     imgsrc='https://images6.alphacoders.com/629/629544.jpg'
+     imgsrc='/images/3.jpg'
      sname='NETFLIX ANIME SERIES'
      ctitle='TOKYO GHOUL'
      clink='https://animeheaven.ru/detail/tokyo-ghoul-dub.85859'
      />
      <Cards
      vlinks='/videos/dbs.mp4'
-     imgsrc='https://images8.alphacoders.com/632/632011.jpg'
+     imgsrc='/images/4.jpg'
      sname='NETFLIX ANIME SERIES'
      ctitle='DRAGON BALL SUPER'
      clink='https://animeheaven.ru/detail/dragon-ball-super-sub.55887'
      />
      <Cards
-     vlinks='/videos/mha.mp4'
-     imgsrc='https://images3.alphacoders.com/714/714324.png'
+     imgsrc='/images/5.jpg'
      sname='NETFLIX ANIME SERIES'
      ctitle='MY HERO ACEDEMIA'
      clink='https://animeheaven.ru/detail/boku-no-hero-academia-sub.37900'
      />
      <Cards
-     vlinks='/videos/a.mp4'
-     imgsrc='https://images.alphacoders.com/612/612521.jpg'
+     imgsrc='/images/6.jpg'
      sname='NETFLIX ANIME SERIES'
      ctitle='ATTACK ON TITAN'
      clink='https://animeheaven.ru/detail/shingeki-no-kyojin-sub.33321'
      />
       <Cards
-      vlinks='/videos/bc.mp4'
-     imgsrc='https://images7.alphacoders.com/106/1061117.jpg'
+     imgsrc='/images/7.jpg'
      sname='NETFLIX ANIME SERIES'
      ctitle='BLACK CLOVER'
      clink='https://animeheaven.ru/detail/black-clover-tv.65233'
      />
     <Cards
-    vlinks='/videos/b.mp4'
-     imgsrc='https://images5.alphacoders.com/331/331316.jpg'
+     imgsrc='/images/8.png'
      sname='NETFLIX ANIME SERIES'
      ctitle='BLEACH'
      clink='https://animeheaven.ru/detail/bleach-sub.63474'
      />
      <Cards
-     vlinks='/videos/sao.mp4'
-     imgsrc='https://images8.alphacoders.com/533/533007.png'
+     imgsrc='/images/9.png'
      sname='NETFLIX ANIME SERIES'
      ctitle='SWORD ART ONLINE'
      clink='https://animeheaven.ru/detail/sword-art-online-alicization-war-of-underworld-sub.94057'
      />
      <Cards
-     vlinks='/videos/tog.mp4'
-     imgsrc='https://wallpapercave.com/wp/wp2127381.png'
+     imgsrc='/images/10.webp'
      sname='NETFLIX ANIME SERIES'
      ctitle='TOWER OF GOD'
      clink='https://animeheaven.ru/detail/kami-no-tou.31768'
